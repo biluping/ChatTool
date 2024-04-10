5a4d5679e199025a285287806426ea75d6701c09
GitHub Action <action@github.com>
2024-04-10 01:39:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/biluping/chattool/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/biluping/chattool/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.biluping.chattool.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.biluping.chattool.MyBundle
+import com.github.biluping.chattool.services.MyProjectService
 import javax.swing.JButton
 
 
